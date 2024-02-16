4688437bb7ff9c9ffce9ea10aa07db7e96f784ab
chihanzheng <charleszheng6778@gmail.com>
2024-02-16 10:05:51 -0500

    Add my version as DatapathSingleCycle_chihan.sv You can make change in the original sv file and should copy paste my code into the original sv file.

--- hw2a/divider_unsigned.sv
+++ hw2a/divider_unsigned.sv
@@ -1,4 +1,10 @@
-/* INSERT NAME AND PENNKEY HERE */
+/* 
+Team member : Feifan Lu
+Penn ID: 13440206
+
+Team member :
+Penn ID: 
+ */
 
 `timescale 1ns / 1ns
 
@@ -12,7 +18,22 @@ module divider_unsigned (
 );
 
     // TODO: your code here
+    wire[31:0] array_dividend[33];
+    wire[31:0] array_remainder[33];
+    wire[31:0] array_quotient[33];
+    assign array_dividend[0] = i_dividend;
+    assign array_remainder[0] = 0;
+    assign array_quotient[0] = 0;
 
+    genvar i;
+    for(i = 0; i < 32; i = i+1) begin
+    divu_1iter d(.i_dividend(array_dividend[i]), .i_divisor(i_divisor), 
+                .i_remainder(array_remainder[i]), .i_quotient(array_quotient[i]),
+                .o_dividend(array_dividend[i+1]), .o_remainder(array_remainder[i+1]),
+                .o_quotient(array_quotient[i+1]));
+    assign o_remainder = array_remainder[32];
+    assign o_quotient = array_quotient[32];
+    end
 endmodule
 
 
@@ -37,7 +58,10 @@ module divu_1iter (
         dividend = dividend << 1;
     }
     */
-
     // TODO: your code here
-
+    assign o_dividend = i_dividend << 1;
+    wire [31:0] temp_remainder;
+    assign temp_remainder = ((i_dividend >> 31) & 1) | (i_remainder << 1);
+    assign o_quotient = (temp_remainder < i_divisor) ? (i_quotient << 1) : ((i_quotient << 1) | 1);
+    assign o_remainder = (temp_remainder < i_divisor) ? (temp_remainder) : (temp_remainder - i_divisor);
 endmodule
--- hw2b/cla.sv
+++ hw2b/cla.sv
@@ -21,12 +21,24 @@ endmodule
  * @param pout whether these 4 bits internally would propagate an incoming carry from cin
  * @param cout the carry outs for the low-order 3 bits
  */
+module gp2(input wire [1:0] gin, pin,
+           input wire cin,
+           output wire pout, gout, cout);
+           assign pout = pin[0] & pin[1];
+           assign gout = (gin[0] & pin[1]) | gin[1];
+           assign cout = (pin[0] & cin) | gin[0];
+endmodule
+
 module gp4(input wire [3:0] gin, pin,
            input wire cin,
            output wire gout, pout,
            output wire [2:0] cout);
 
    // TODO: your code here
+           wire [1:0] ptemp, gtemp;
+           gp2 g1(.gin(gin[1:0]), .pin(pin[1:0]), .cin(cin), .pout(ptemp[0]), .gout(gtemp[0]), .cout(cout[0]));
+           gp2 g2(.gin(gin[3:2]), .pin(pin[3:2]), .cin(cout[1]), .pout(ptemp[1]), .gout(gtemp[1]), .cout(cout[2]));
+           gp2 g3(.gin(gtemp), .pin(ptemp), .cin(cin), .pout(pout), .gout(gout), .cout(cout[1]));
 
 endmodule
 
@@ -37,7 +49,10 @@ module gp8(input wire [7:0] gin, pin,
            output wire [6:0] cout);
 
    // TODO: your code here
-
+           wire [1:0] ptemp, gtemp;
+           gp4 g1(.gin(gin[3:0]), .pin(pin[3:0]), .cin(cin), .gout(gtemp[0]), .pout(ptemp[0]), .cout(cout[2:0]));
+           gp4 g2(.gin(gin[7:4]), .pin(pin[7:4]), .cin(cout[3]), .gout(gtemp[1]), .pout(ptemp[1]), .cout(cout[6:4]));
+           gp2 g3(.gin(gtemp), .pin(ptemp), .cin(cin), .pout(pout), .gout(gout), .cout(cout[3]));
 endmodule
 
 module cla
@@ -46,5 +61,24 @@ module cla
    output wire [31:0] sum);
 
    // TODO: your code here
+   wire[31:0] gin, pin;
+   wire [7:0] ptemp, gtemp;
+   wire [32:0] cout;
+   wire [6:0] ctemp;
+   wire pout, gout;
 
+   assign cout[0] = cin;
+   genvar i;
+   for (i = 0; i < 8;i=i+1) begin
+   gp4 g(.gin(gin[4*i+3:4*i]), .pin(pin[4*i+3:4*i]), .cin(cout[4*i]), .gout(gtemp[i]), .pout(ptemp[i]), .cout(cout[4*i+3:4*i+1]));
+   end
+   for (i = 0; i < 7;i=i+1) begin
+   assign cout[4*(i+1)] = ctemp[i];
+   end
+   gp8 gp81(.gin(gtemp), .pin(ptemp), .cin(cin), .pout(pout), .gout(gout), .cout(ctemp));
+   assign cout[32] = gout | (pout & cin);
+   for (i = 0; i < 32;i=i+1) begin
+   gp1 gp11(.a(a[i]), .b(b[i]), .g(gin[i]), .p(pin[i]));
+   assign sum[i] = (a[i] ^ b[i] ^ cout[i]);
+   end
 endmodule
